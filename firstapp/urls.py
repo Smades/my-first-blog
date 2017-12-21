d5b483bf0fd1e4bd863059f38e00a1d695a04729
Joris Nieuwkoop <jorisnew14@gmail.com>
2017-12-21 21:03:40 +0100

    import login

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^learnmore.html$', views.learnmore, name='learnmore'),
+    url(r'^accounts/login/$', login, {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', logout),
 ]
